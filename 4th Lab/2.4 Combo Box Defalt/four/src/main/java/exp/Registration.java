2cca47f06b3ce03d4d511d6a9418a6506443a144
Rezwan <rezwanahmedtorabee@gmail.com>
2026-09-03 00:24:34 +0600

    Combo Box Defalt

---
+++ 4th Lab/2.4 Combo Box Defalt/four/src/main/java/exp/Registration.java
@@ -0,0 +1,32 @@
+package exp;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Registration extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        ComboBox<String> locationCombo = new ComboBox<>();
+
+        locationCombo.getItems().addAll("Dhaka", "Saidpur", "Chittagong", "Rajshahi", "Khulna");
+
+        locationCombo.setValue("Dhaka"); // Set default value
+
+        VBox root = new VBox(10, locationCombo);
+        Scene scene = new Scene(root, 300, 200);
+
+        primaryStage.setTitle("Step 3: Using addAll");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
