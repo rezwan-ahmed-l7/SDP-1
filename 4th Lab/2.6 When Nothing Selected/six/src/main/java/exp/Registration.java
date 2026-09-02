a3f45ba102939bd564e82a7bb83810c857a4292f
Rezwan <rezwanahmedtorabee@gmail.com>
2026-09-03 00:28:25 +0600

    SDP

---
+++ 4th Lab/2.6 When Nothing Selected/six/src/main/java/exp/Registration.java
@@ -0,0 +1,48 @@
+package exp;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Registration extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        ComboBox<String> locationCombo = new ComboBox<>();
+
+        locationCombo.getItems().addAll("Dhaka", "Saidpur", "Chittagong", "Rajshahi", "Khulna");
+
+        locationCombo.setValue("Dhaka");
+
+        Button showButton = new Button("Show Selected Location");
+
+        Label resultLabel = new Label();
+
+        showButton.setOnAction(e -> {
+            String selectedLocation = locationCombo.getValue();
+
+            if (selectedLocation == null) { // Check if a location is selected
+                resultLabel.setText("Please select a location first");
+            } else {
+                resultLabel.setText("You selected: " + selectedLocation);
+            }
+        });
+
+        VBox root = new VBox(10, locationCombo, showButton, resultLabel);
+        Scene scene = new Scene(root, 300, 250);
+
+        primaryStage.setTitle("Step 3: Using addAll");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
