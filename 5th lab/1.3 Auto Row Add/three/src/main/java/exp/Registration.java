70f2ddfa1d0f02345aec79f9ed7897073cc470fe
Rezwan <rezwanahmedtorabee@gmail.com>
2026-09-06 01:58:49 +0600

    Auto Row add

---
+++ 5th lab/1.3 Auto Row Add/three/src/main/java/exp/Registration.java
@@ -0,0 +1,90 @@
+package exp;
+
+import javafx.application.Application;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.layout.VBox;
+
+// A simple Person class to hold data for the TableView
+class Person {
+    String name;
+    int age;
+
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+public class Registration extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        // Create a TableView and add columns to connect to the Person class properties
+        TableView<Person> table = new TableView<>();
+
+        TableColumn<Person, String> col1 = new TableColumn<>("Name");
+        TableColumn<Person, Integer> col2 = new TableColumn<>("Age");
+
+        // Bind the properties of the Person class to the columns to get the data
+        col1.setCellValueFactory(cellData -> {
+            Person p = cellData.getValue();
+            return new SimpleStringProperty(p.name);
+        });
+
+        col2.setCellValueFactory(cellData -> {
+            Person p = cellData.getValue();
+            return new SimpleIntegerProperty(p.age).asObject();
+        });
+
+        table.getColumns().addAll(col1, col2);
+
+        // Add data to the TableView
+        ObservableList<Person> data = FXCollections.observableArrayList();
+        data.add(new Person("Paris", 25));
+        data.add(new Person("Rex", 30));
+        data.add(new Person("Prince", 28));
+
+        table.setItems(data);
+
+        // Create input fields
+        TextField nameField = new TextField();
+        nameField.setPromptText("Enter name");
+
+        TextField ageField = new TextField();
+        ageField.setPromptText("Enter age");
+
+        Button addButton = new Button("Add Person");
+        addButton.setOnAction(e -> {
+            String name = nameField.getText();
+            int age = Integer.parseInt(ageField.getText());
+            data.add(new Person(name, age));
+            nameField.clear();
+            ageField.clear();
+        });
+
+        // Create VBox layout
+        VBox root = new VBox(10, table, nameField, ageField, addButton);
+
+        // Create Scene
+        Scene scene = new Scene(root, 400, 400);
+
+        // Setup Stage
+        primaryStage.setTitle("My Table");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
